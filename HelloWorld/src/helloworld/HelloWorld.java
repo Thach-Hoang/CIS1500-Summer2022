708912729bfa09db48d075abb5886045b92c30c0
Thach-Hoang <hoangngocthach1996@gmail.com>
2022-05-13 05:53:28 -0400

    multiple s.o.u.t instead of 1

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -3,6 +3,12 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("My partner is Dan Youngs. ");
+        System.out.println("His major is software engineering.");
+        System.out.println("He is currently in a program called \"future for frontliners\" which is offered to frontline workers during the pandemic.");
+        System.out.println("Dan has 2 or 3 semesters left now in OCC. He has a lot of hobbies including arts like writing and recording music, tattoos, and programming video games.");
+        System.out.println("For reference, Dan has made an 8bit, Pee Wee Herman beat 'em up game last semester and plan to continue working on his game this Summer, so give him a round of applause!");
+
     }
+
 }
